ff8b5596ef97b16354e52bc669247320ebfc5190
mshawish <mshawish@DESKTOP-JJQOTR8>
2021-07-06 13:17:03 +0300

    Add project files.

---
+++ ClinicTraining/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using ClinicTraining.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicTraining.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Anything";
+            ViewBag.Customer = new Customer();
+            TempData["Date"] = DateTime.Today;
+            ViewData["Title"] = "Just Test";
+            return View("Main");
+        }
+        [HttpPost]
+        public string Section(int id, string title)
+        {
+            return $"You passed {id} and title {title}";
+        }
+
+        public IActionResult Main()
+        {
+            return View("Main");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
